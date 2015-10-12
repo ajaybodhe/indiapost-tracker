1fc8e0337cfcaaca1a204b3b559ec606fe042fa6
ajaybodhe <ajaybodhe@gmail.com>
2015-10-12 19:08:13 +0530

    fixing indian post api

--- tracker.py
+++ tracker.py
@@ -3,11 +3,21 @@ from bs4 import BeautifulSoup
 import tempfile
 import os
 from dateutil import parser
+import datetime
 import json
 
-HOME_URL="http://www.indiapost.gov.in/speednettracking.aspx"
+#HOME_URL="http://www.indiapost.gov.in/speednettracking.aspx"
+HOME_URL="http://www.indiapost.gov.in/ParcelNetTracking.aspx"
 CAPTCHA_URL="http://www.indiapost.gov.in/captcha.aspx"
 ROOT_URL = "http://www.indiapost.gov.in/"
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            encoded_object = list(obj.timetuple())[0:6]
+        else:
+            encoded_object =json.JSONEncoder.default(self, obj)
+        return encoded_object
 
 class Tracker:
   def __init__(self):
@@ -16,7 +26,7 @@ class Tracker:
     self.session = requests.Session()
     home_response = self.session.get(HOME_URL)
 
-    dom = BeautifulSoup(home_response.content)
+    dom = BeautifulSoup(home_response.content, "lxml")
 
     inputs = dom.find_all('input')
 
@@ -35,28 +45,33 @@ class Tracker:
 
     captcha_file.close()
     self.POST_DATA['txtCaptcha'] = os.popen("python captcha.py "+captcha_file.name).read().strip()
+#print self.POST_DATA['txtCaptcha']
     os.remove(captcha_file.name)
 
   def track(self, id):
     details = {}
     self.POST_DATA['Txt_ArticleTrack'] = id
     response = self.session.post(HOME_URL, data=self.POST_DATA)
-    dom = BeautifulSoup(response.content)
+#   print response
+    dom = BeautifulSoup(response.content, "lxml")
+#print dom
 
     general_details = dom.find(id="GridView1").findAll('td')
 
     if len(general_details) < 7:
       return None
 
-    details['id'] = dom.find(id='Label1').text.strip()
-    details['origin'] = general_details[0].text.strip()
-    details['booking_date'] = parser.parse(general_details[1].text.strip())
-    details['pincode'] = general_details[2].text.strip()
-    details['tariff'] = general_details[3].text.strip()
-    details['category'] = general_details[4].text.strip()
-    details['destination'] = general_details[5].text.strip()
-    details['delivery_date'] = general_details[6].text.strip()
-    details['delivered'] = (details['delivery_date'] != 'Not Available')
+    details['id'] = dom.find(id='Lbl_Track1').text.strip()
+#print details['id']
+    details['Booked at'] = general_details[0].text.strip()
+#print details['Booked at'] 
+    details['Booked on'] = parser.parse(general_details[1].text.strip())
+    details['Destination'] = general_details[2].text.strip()
+    details['Tariff'] = general_details[3].text.strip()
+    details['Article Type'] = general_details[4].text.strip()
+    details['Delivered at'] = general_details[5].text.strip()
+    details['Paid on'] = general_details[6].text.strip()
+    details['delivered'] = (details['Delivered at'] != 'Not Available')
 
     details['events'] = []
 
@@ -64,14 +79,20 @@ class Tracker:
     for tr in events:
       event = {}
       data = tr.findAll('td')
-      event['date'] = parser.parse(data[0].text.strip() + ' ' + data[1].text.strip() + ' IST')
+#event['date'] = parser.parse(data[0].text.strip() + ' ' + data[1].text.strip() + ' IST')
+      event['date'] = data[0].text.strip()
       event['office'] = data[2].text.strip()
       event['description'] = data[3].text.strip()
 
       details['events'].append(event)
 
+# print details
     return details
 
 if __name__ == '__main__':
-  tracker = Tracker()
-  print json.dumps(tracker.track("AW005912565IN"), cls=DateTimeEncoder, sort_keys=True, indent=4, separators=(',', ': '))
+  orderList = []
+  orderList.append("CG003693164IN")
+  orderList.append("CG002825473IN")
+  for orderID in orderList:
+    tracker = Tracker()
+    print json.dumps(tracker.track(orderID), cls=DateTimeEncoder, sort_keys=True, indent=4, separators=(',', ': '))
